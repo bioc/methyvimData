aeefa5fcb0b7e7b6d79a98ce978762447b52282c
Nima Hejazi <nh@nimahejazi.org>
2017-08-07 11:11:43 -0700

    initial

---
+++ R/grsExample.R
@@ -0,0 +1,19 @@
+#' Filtered Mock Epigenomics Data Set
+#'
+#' A data set containing data that is similar in structure to that typically
+#' found in epigenomics studies.
+#'
+#' \describe{
+#'   This data set was simulated based on several key characteristics of data
+#'   sets available to the package authors through collaborations in studies of
+#'   differential methylation. While the motivating data sets were collected on
+#'   the Infinium MethylationEPIC BeadChip (Illumina) platform, the data has
+#'   been reduced for convenience purposes. We emphasize that this is not real
+#'   data and is not in any way suitable for scientific investigation. Please
+#'   note that THIS IS NOT REAL DATA.
+#' }
+#'
+#' @return A \code{GenomicRatiSet} containing pre-filtered CpG sites, ready to
+#'         be used with the core function of the \code{methyvim} package.
+#'
+"grsExample"
