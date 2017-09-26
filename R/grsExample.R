81308fde3705f500c2ee69c4c2643ed8040446e0
Nima Hejazi <nh@nimahejazi.org>
2017-09-26 13:49:02 -0700

    fix minfi depends, more detail in data, update version

--- R/grsExample.R
+++ R/grsExample.R
@@ -6,14 +6,21 @@
 #' \describe{
 #'   This data set was simulated based on several key characteristics of data
 #'   sets available to the package authors through collaborations in studies of
-#'   differential methylation. While the motivating data sets were collected on
-#'   the Infinium MethylationEPIC BeadChip (Illumina) platform, the data has
-#'   been reduced for convenience purposes. We emphasize that this is not real
-#'   data and is not in any way suitable for scientific investigation. Please
-#'   note that THIS IS NOT REAL DATA.
+#'   differential methylation. It is based on data collected using the 850K
+#'   (EPIC) Infinium arrays produced by Illumina, from samples collected on
+#'   human subjects in studies of environmental epidemiology. The structure of
+#'   the original data set (e.g., notable differences between the exposure and
+#'   treatment conditions) were used to guide the simulation, and data was only
+#'   produced for an arbitrary subset of sites. The phenotype-level exposure and
+#'   outcome data (accessible via \code{colData}) were derived from the original
+#'   data. The size of the subset was chosen such that the data package would be
+#'   compliant with the standards outlined by Bioconductor. We emphasize that
+#'   this is not real data and is not in any way suitable for scientific
+#'   investigation. Please note that THIS IS NOT REAL DATA.
 #' }
 #'
-#' @return A \code{GenomicRatiSet} containing pre-filtered CpG sites, ready to
+#' @return A \code{GenomicRatioSet} containing pre-filtered CpG sites, ready to
 #'         be used with the core function of the \code{methyvim} package.
-#'
+#
 "grsExample"
+
